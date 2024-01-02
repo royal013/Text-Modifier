872678d201be8c7e5a418fee90310f9b56cf12b6
Royal <royalbista001@gmail.com>
2024-01-02 17:47:58 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,118 @@
+const textarea = document.getElementById("textarea1");
+let optionsButtons = document.querySelectorAll(".option-button");
+let advancedOptionButton = document.querySelectorAll(".adv-option-button");
+let fontName = document.getElementById("fontName");
+
+function f1(e) {
+    let value = e.value;
+    textarea.style.fontSize = value + "px";
+}
+
+function f2(e) {
+    if (textarea.style.fontWeight == "bold") {
+        textarea.style.fontWeight = "normal";
+        e.classList.remove("active");
+    }
+    else {
+        textarea.style.fontWeight = "bold";
+        e.classList.add("active");
+    }
+}
+
+function f3(e) {
+    if (textarea.style.fontStyle == "italic") {
+        textarea.style.fontStyle = "normal";
+        e.classList.remove("active");
+    }
+    else {
+        textarea.style.fontStyle = "italic";
+        e.classList.add("active");
+    }
+}
+
+function f4(e) {
+    if (textarea.style.textDecoration == "underline") {
+        textarea.style.textDecoration = "none";
+        e.classList.remove("active");
+    }
+    else {
+        textarea.style.textDecoration = "underline";
+        e.classList.add("active");
+    }
+}
+
+function f5(e) {
+    textarea.style.textAlign = "left";
+}
+
+function f6(e) {
+    textarea.style.textAlign = "center";
+}
+
+function f7(e) {
+    textarea.style.textAlign = "right";
+}
+
+function f8(e) {
+    if (textarea.style.textTransform == "uppercase") {
+        textarea.style.textTransform = "none";
+        e.classList.remove("active");
+    }
+    else {
+        textarea.style.textTransform = "uppercase";
+        e.classList.add("active");
+    }
+}
+
+function f9() {
+    textarea.style.fontWeight = "normal";
+    textarea.style.textAlign = "left";
+    textarea.style.fontStyle = "normal";
+    textarea.style.textTransform = "capitalize";
+    textarea.value = "";
+}
+
+function f10(e) {
+    let value = e.value;
+    textarea.style.color = value;
+}
+
+window.addEventListener('load', () => {
+    textarea.value = "";
+});
+
+
+
+
+
+
+//For basic operations which don't need value parameter
+optionsButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        modifyText(button.id, false, null);
+    });
+});
+
+//options that require value parameter (e.g colors, fonts)
+advancedOptionButton.forEach((button) => {
+    button.addEventListener("change", () => {
+        modifyText(button.id, false, button.value);
+    });
+});
+
+function changeFont() {
+    var fontSelector = document.getElementById('fontSelector');
+    var selectedFont = fontSelector.options[fontSelector.selectedIndex].value;
+
+    var textInput = document.getElementById('textarea1');
+    textInput.style.fontFamily = selectedFont;
+}
+
+
+window.onload = initializer();
+
+
+
+
+
+
